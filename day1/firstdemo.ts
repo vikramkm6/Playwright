dc7670e4c78e6f783f00163411358a696df27ed3
vikramkm6 <vikrammehta6582@gmail.com>
2026-04-23 22:46:06 +0530

    First farhan commit

---
+++ day1/firstdemo.ts
@@ -0,0 +1,48 @@
+
+
+// // function scope()
+// // {
+// //     if(true)
+// //         {
+// //         const a= 10
+// //         console.log(a);
+// // }
+// //     }
+// //    //console.log(a);
+
+// // scope()
+
+
+
+// // let data:number =10
+// // data = 'ten'
+// // console.log(data);
+
+// // const message = 'hello'
+// // let count = 30;
+
+// // let message = 100;
+// // console.log(message);
+
+// let age:number =100
+// let price:number =30
+// console.log("Age : ",age )
+// console.log(price )
+
+// console.log("Print type of ",typeof(price))
+
+// let str = "Vikram";
+// let reversed = "";
+
+// for (let i = str.length - 1; i >= 0; i--) {
+//     reversed += str[i];
+// }
+
+// console.log("Reversed String:", reversed);
+
+
+let numbers: number[] = [10, 20, 30];
+
+for (let num of numbers) {
+    console.log(num);
+}
